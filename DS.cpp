a35170b16a86d8ddc1f4080d16ebbcc36c68a6c3
sam <153282803+bsdwiki@users.noreply.github.com>
2024-03-27 21:09:28 +0800

    misc

--- DS.cpp
+++ DS.cpp
@@ -1,3 +1,17 @@
-//
-// Created by sam on 3/27/24.
-//
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+struct type_name {
+    int member_name1;
+    char member_name2;
+} object_names;
+int main{
+        type_name object_names1;
+        type_name object_names2;
+        cout << " member_name: " << object_names1.member_name1 << endl;
+        cout << " member_name: " << object_names2.member_name2 << endl;
+
+
+        return 0;
+}
